4d908d0f9edc966e2c808a09a6e040ce5c558e94
anhadbatra <42717926+anhadbatra@users.noreply.github.com>
2021-02-25 17:40:41 +0530

    Add files via upload

---
+++ test_main.py
@@ -0,0 +1,6 @@
+def Add(a,b):
+    return a+b
+def SayHello():
+    print("This is a test file")
+if __name__=='__main__':
+    SayHello()
---
+++ test_main2.py
@@ -0,0 +1,6 @@
+from test_main import Add
+def TestAdd():
+    assert Add(2,3)==5
+    print("This is the added value")
+if __name__=='__main__':
+    TestAdd()
